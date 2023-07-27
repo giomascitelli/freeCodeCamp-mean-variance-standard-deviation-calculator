9c374a2e25f4d731fd15a7f5cb44660fb570cde9
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-07-27 19:40:31 +0000

    This is the first project out of five that I completed as part of the 'Data Analysis with Python' course from freeCodeCamp.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Mean-Variance-Standard Deviation Calculator
 
-This is the boilerplate for the Mean-Variance-Standard Deviation Calculator project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/data-analysis-with-python/data-analysis-with-python-projects/mean-variance-standard-deviation-calculator
+This is the first project out of five that I completed as part of the 'Data Analysis with Python' course from freeCodeCamp. For this project, I worked on Replit using their test code to develop a function. The function converts a 9 digits list into a 3x3 Numpy array and then return the mean, variance, standard deviation, max, min and sum of the rows, columns and elements inside the matrix.
--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,36 @@
 import numpy as np
 
-def calculate(list):
+def calculate(nums):
 
+    # Handling error
+    if len(nums) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    # Converting the original list into an Numpy array and then 'reshape' it into a 3x3 matrix
+    matrix = np.array(nums).reshape(3, 3) 
 
+    # Calculations for both axes and for the flattened matrix
+    mean = [list(matrix.mean(axis=0)), list(matrix.mean(axis=1)), matrix.mean()]
+    
+    variance = [list(matrix.var(axis=0)), list(matrix.var(axis=1)), matrix.var()]
+    
+    std_deviation = [list(matrix.std(axis=0)), list(matrix.std(axis=1)), matrix.std()]
+    
+    max_num = [list(matrix.max(axis=0)), list(matrix.max(axis=1)), matrix.max()]
+    
+    min_num = [list(matrix.min(axis=0)), list(matrix.min(axis=1)), matrix.min()]
+    
+    sum_nums = [list(matrix.sum(axis=0)), list(matrix.sum(axis=1)), matrix.sum()]
 
+    # The result dictionary
+    calculations = {
+        'mean': mean,
+        'variance': variance,
+        'standard deviation': std_deviation,
+        'max': max_num,
+        'min': min_num,
+        'sum': sum_nums
+    }
+    
     return calculations
+
